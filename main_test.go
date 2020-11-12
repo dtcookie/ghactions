bece10b74a33114fef38655d63465a5daf8d7dd8
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2020-11-12 10:08:05 +0100

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -3,7 +3,18 @@ package main_test
 import "testing"
 
 func TestSum(t *testing.T) {
+	t.Log("executing", "TestSum")
 }
 
-func TestSum2(t *testing.T) {
+func TestMin(t *testing.T) {
+	t.Log("executing", "TestMin")
 }
+
+// return func(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+// 	if file, e := os.OpenFile("terraform-provider-dynatrace.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666); e != nil {
+// 		panic(errors.New("Failed to open log file"))
+// 	} else {
+// 		log.SetOutput(file)
+// 	}
+// 	return fn(ctx, d, m)
+// }
